ece09d211ddf196174ca4447666d9057b20b758e
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-11-08 16:42:56 +0530

    filtering added

--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -6,13 +6,21 @@ import Pagination from '@material-ui/lab/Pagination'
 function ListCard() {
     const [data, setData] = React.useState([]);
     const [page, setPage] = React.useState(1);
+    const [filterType, setFilterType] = React.useState()
    
     function handlePage(e) {
-        setPage(e.target.textContent)
-       
-        
+        setPage(e.target.textContent)    
     }
-  
+      function getfilterApiData(filterType,page) {
+        axios
+          .get(
+            `https://rickandmortyapi.com/api/character/?page=${page}&status=${filterType}`
+          )
+          .then((response) => {
+            console.log(response);
+            setData(response.data.results);
+          });
+      }
     function getPaginationApiData(page) {
       
        axios
@@ -37,6 +45,10 @@ function ListCard() {
     React.useEffect(() => {
        getApiData();
     }, [])
+
+       React.useEffect(() => {
+         getfilterApiData(filterType, page);
+       }, [filterType,page]);
     
 
     return (
@@ -47,9 +59,10 @@ function ListCard() {
           onChange={handlePage}
           size="large"
         />
-        <button>alive</button>
-        <button>dead</button>
-        <button>unKnown</button>
+        <button onClick={() => setFilterType("alive")}>alive</button>
+        <button onClick={() => setFilterType("dead")}>dead</button>
+        <button onClick={() => setFilterType("unknown")}>unKnown</button>
+
         {data.map((val) => {
           return (
             <div
