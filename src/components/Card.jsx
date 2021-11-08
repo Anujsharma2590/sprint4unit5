dfd56ad6222734c94f1a00352fcbf6506cb12190
Anuj sharma <aanuj.sharma2590@gmail.com>
2021-11-08 15:44:21 +0530

    card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 
-function Card() {
+function Card({ name,status,species,type,gender,image}) {
+
     return (
-        <div style={{border : "1px solid black"}}>
-            
-            
+      <div style={{ border: "1px solid black", display:"flex", width:"30%" }}>
+        <div>
+          <img src={image} height={"100px"} width={"100px"} alt="" />
         </div>
-    )
+        <div>
+          <div> Name : {name}</div>
+          <div> Gender : {gender}</div>
+          <div>Status : {status}</div>
+          <div>Species : {species}</div>
+          <div>Type : {type}</div>
+        </div>
+      </div>
+    );
 }
 
 export default Card
--- src/components/ListCard.jsx
+++ src/components/ListCard.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
+import Card from './Card'
+import axios from 'axios'
+import Pagination from '@material-ui/lab/Pagination'
 
 function ListCard() {
     const [data, setData] = React.useState([]);
-
+    const [page, setPage] = React.useState('');
+    console.log(page);
     function getApiData() {
-         axios.get('https://rickandmortyapi.com/api/character')
-             .then((response) => {
-                 console.log(response)
-                 setData(response.data);
-         })
+        fetch("https://rickandmortyapi.com/api/character")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.results)
+                setData(data.results)
+        })
+        //  axios.get('https://rickandmortyapi.com/api/character')
+        //      .then((response) => {
+        //          console.log(response)
+        //          setData(response.data);
+        //  })
      }
     React.useEffect(() => {
         getApiData();
   },[])
     return (
-        <div>
-            hello world
+        <div>   
+       
+            {data.map((val) => {
+                return <div key={ val.id} style = {{margin:"10px"}}>
+                    <Card {...val}/>
+                    </div>
+            })}
+            <Pagination count={20} page={ page} />
         </div>
     )
 }
